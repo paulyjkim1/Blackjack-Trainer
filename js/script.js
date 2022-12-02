09c0e61a053741403f341787237d78295a8005fd
paulyjkim1 <paulyjkim1@gmail.com>
2022-12-01 17:30:28 -0800

    create deck

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const suits = ["♥", "♦", "♠", "♣"] 
+const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+const deck= []
+
+for(const suit of suits){
+    for(const number of numbers){
+        deck.push(number + suit)
+    }
+}
+
+console.log(deck)
